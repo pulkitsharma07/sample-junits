2dfd586136175811511d9c571a96903832860d60
Anirudha Khanna <anirudha_81@yahoo.com>
2016-06-06 18:42:11 +0530

    Added a failing unit test.

---
+++ src/test/java/my/anirudha/SampleTest.java
@@ -0,0 +1,27 @@
+package my.anirudha;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Anirudha Khanna
+ */
+public class SampleTest {
+
+    @Test
+    public void testThatPasses() throws Exception {
+        Assert.assertTrue("This should pass", true);
+    }
+
+    @Test
+    public void anotherTestThatPasses() throws Exception {
+        Assert.assertEquals("This should pass", 100, 100);
+    }
+
+    @Test
+    public void testThatFails() throws Exception {
+        Assert.assertFalse("This will always fail", true);
+    }
+}
